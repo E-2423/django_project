2dc8ddafc3de5e0a8205321d0957bead27cd05fb
Joseph Abraham <ekurubal24@gmail.com>
2020-12-28 20:05:48 +0300

    Added model forms

--- src/fscohort/forms.py
+++ src/fscohort/forms.py
@@ -1,8 +1,16 @@
 from django import forms
+from fscohort.models import Student
 
 
 
-class StudentForm(forms.Form):
-    first_name =  forms.CharField(max_length=50)
-    last_name = forms.CharField(max_length=50)
-    number = forms.IntegerField()
+# class StudentForm(forms.Form):
+#     first_name =  forms.CharField(max_length=50)
+#     last_name = forms.CharField(max_length=50)
+#     number = forms.IntegerField()
+
+class StudentForm(forms.ModelForm):
+    
+    class Meta:
+        model = Student
+        fields = {'first_name', 'last_name'}
+        
