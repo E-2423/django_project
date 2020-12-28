a4c412110133ac4397d47cf23fa3464b421805c7
Joseph Abraham <ekurubal24@gmail.com>
2020-12-28 21:38:26 +0300

    Created base page

--- src/fscohort/urls.py
+++ src/fscohort/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from fscohort.views import home_view, about
+from fscohort.views import home_view
 
 urlpatterns = [
     path("", home_view),
-    path("about/", about)
+    # path("about/", about)
 ]
