c9678de1d09a85079fc1e893a8e6aa7ddbdfd479
Joseph Abraham <ekurubal24@gmail.com>
2020-12-28 21:54:24 +0300

    added student_list

--- src/fscohort/urls.py
+++ src/fscohort/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from fscohort.views import home_view
+from fscohort.views import home_view, student_list
 
 urlpatterns = [
     path("", home_view),
-    # path("about/", about)
+    path("list/", student_list)
 ]
--- src/fscohort/views.py
+++ src/fscohort/views.py
@@ -1,23 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import StudentForm
+from fscohort.models import Student
 
 
 def home_view(request):
     # return HttpResponse("This is about page")
     
-    form = StudentForm()
+    # form = StudentForm()
     
-    my_context = {
-        'title': '<b>Clarusway</b>',
-        'dict_1': {'django':'best framework'},
-        'my_list': [2, 3, 4, 5],
-        'form': form
+    # my_context = {
+    #     'title': '<b>Clarusway</b>',
+    #     'dict_1': {'django':'best framework'},
+    #     'my_list': [2, 3, 4, 5],
+    #     'form': form
         
+    # }
+    return render(request, "fscohort/home.html")
+
+def student_list(request):
+    students = Student.objects.all()
+    context ={
+        'students': students
     }
-    return render(request, "fscohort/home.html", my_context)
+    
+    return render(request, "fscohort/student_list.html", context)
 
-def about(request):
-    return HttpResponse("About Page")
 
-# Create your views here.
+
+
