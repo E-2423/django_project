89aa490f172a7d30c5d340355f860dc680d44b66
Joseph Abraham <ekurubal24@gmail.com>
2020-12-28 19:18:12 +0300

    Added home page

--- src/fscohort/views.py
+++ src/fscohort/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 def home_view(request):
     # return HttpResponse("This is about page")
     my_context = {
-        'title': 'Clarussway',
+        'title': '<b>Clarussway</b>',
         'dict_1': {'django':'best framework'},
         'my_list': [2, 3, 4, 5]
     }
