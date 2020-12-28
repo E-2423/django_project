c1da484e8af539554c2108831ccbd109e4f4ed7f
Joseph Abraham <ekurubal24@gmail.com>
2020-12-28 20:24:38 +0300

    added __init__override

--- src/fscohort/forms.py
+++ src/fscohort/forms.py
@@ -4,13 +4,26 @@ from fscohort.models import Student
 
 
 # class StudentForm(forms.Form):
-#     first_name =  forms.CharField(max_length=50)
-#     last_name = forms.CharField(max_length=50)
+#     first_name =  forms.CharField(max_length=50, label="Your Name")
+#     last_name = forms.CharField(max_length=50, labe="Your surname")
 #     number = forms.IntegerField()
 
 class StudentForm(forms.ModelForm):
     
+    # first_name = forms.CharField(label="Your Name")
+    
     class Meta:
         model = Student
-        fields = {'first_name', 'last_name'}
+        fields ='__all__'  #{'first_name', 'last_name'}
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.fields['first_name'].label= 'My Name'
+        
+
+    
+    
+        
+        
         
