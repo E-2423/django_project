21a408767f97ab659c9afe0893e25635ac5ff1a3
Joseph Abraham <ekurubal24@gmail.com>
2020-12-29 12:49:28 +0300

    Added student detail

--- src/fscohort/urls.py
+++ src/fscohort/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from fscohort.views import home_view, student_add, student_list
+from fscohort.views import home_view, student_add, student_list, student_detail
 
 urlpatterns = [
-    path("", home_view),
-    path("list/", student_list),
-    path("add/", student_add),
+    path("", home_view, name="home"),
+    path("list/", student_list, name="list"),
+    path("add/", student_add, name="add"),
+    path("<int:id>", student_detail, name="detail"),
 ]
